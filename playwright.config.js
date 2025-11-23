986c49225379743be3b06c877038f5eb8def6dd6
Dariyoig <darioignatov72@gmail.com>
2025-11-23 12:47:10 +0200

    chore: create initial npm and playwright configuration

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+// Playwright configuration file for test infrastructure (ES module format)
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  timeout: 30000,
+  retries: 1,
+  use: {
+    baseURL: "http://training.skillo-bg.com:4300",
+    headless: false,
+    screenshot: "on",
+    video: "retain-on-failure",
+    trace: "on",
+  },
+  reporter: [["html", { open: "never" }]],
+  projects: [
+    { name: "Chromium", use: { browserName: "chromium" } },
+    { name: "Firefox", use: { browserName: "firefox" } },
+    { name: "WebKit", use: { browserName: "webkit" } },
+  ],
+  workers: 1,
+});
