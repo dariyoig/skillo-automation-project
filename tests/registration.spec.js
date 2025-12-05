a4309ec040e2c61f7cbb83e6e56195af680767d1
Dariyoig <darioignatov72@gmail.com>
2025-12-05 20:33:13 +0200

    feat: initial dev of registration tests

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -1,6 +1,69 @@
 // // Registration tests specification file
-// Test 1 - Verify successfull registration message
-// Test 2 - Verify correct user profile after registration
-// Test 3 - Verify sign in button inactivity in case of missing input
-// Test 4 - Verify sign in button inactivity in case of wrong input
-// Test 5 - Verify error message in case of existing username
+// Test 1 - Verify successfull registration
+// Test 2 - Verify sign in button inactivity in case of missing input
+// Test 3 - Verify sign in button inactivity in case of wrong input
+
+import { test, expect } from "@playwright/test";
+import { RegistrationPage } from "../pages/RegistrationPage";
+import registrationData from "../test-data/registrationData.json" assert { type: "json" };
+import { LoginPage } from "../pages/LoginPage";
+
+const missingInputs = registrationData.missingInputs;
+const invalidInputs = registrationData.invalidInputs;
+
+test.describe("Successful registration suite", () => {
+  test("Verify successfull registration", async ({ page }) => {
+    const registrationPage = new RegistrationPage(page);
+    const loginPage = new LoginPage(page);
+
+    const validInput = { ...registrationData.validInputs[0] };
+    validInput.username += Date.now();
+
+    await loginPage.goToBasePage();
+    await loginPage.click(loginPage.loginButtonLocator);
+    await loginPage.click(loginPage.registerButtonLocator);
+
+    await registrationPage.fillRegistrationForm(validInput);
+    await registrationPage.submitRegistrationForm();
+
+    await expect(
+      registrationPage.successfullRegisterMessageLocator
+    ).toBeVisible();
+  });
+});
+
+test.describe("Unsuccessful registration suite", () => {
+  missingInputs.forEach((dataSet) => {
+    test(`Verify sign in button inactivity in case of ${dataSet.case}`, async ({
+      page,
+    }) => {
+      const registrationPage = new RegistrationPage(page);
+      const loginPage = new LoginPage(page);
+      await loginPage.goToBasePage();
+      await loginPage.click(loginPage.loginButtonLocator);
+      await loginPage.click(loginPage.registerButtonLocator);
+
+      await registrationPage.fillRegistrationForm(dataSet);
+
+      await expect(registrationPage.regSigninButtonLocator).toBeDisabled();
+      //await expect(page.url()).toContain(registrationPage.url);
+    });
+  });
+
+  invalidInputs.forEach((dataSet) => {
+    test(`Verify sign in button inactivity in case of ${dataSet.case}`, async ({
+      page,
+    }) => {
+      const registrationPage = new RegistrationPage(page);
+      const loginPage = new LoginPage(page);
+      await loginPage.goToBasePage();
+      await loginPage.click(loginPage.loginButtonLocator);
+      await loginPage.click(loginPage.registerButtonLocator);
+
+      await registrationPage.fillRegistrationForm(dataSet);
+
+      await expect(registrationPage.regSigninButtonLocator).toBeDisabled();
+      //await expect(page.url()).toContain(registrationPage.url);
+    });
+  });
+});
