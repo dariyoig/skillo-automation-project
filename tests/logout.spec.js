da97cc270fa0f5e9a04e5acc6146e400412496d2
Dariyoig <darioignatov72@gmail.com>
2025-12-06 18:20:15 +0200

    feat: added one more test for logout after registration

--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,12 +1,15 @@
 // // Logout tests specification file
-//Test 1 - Verify user is logged out when pressing exit button
+//Test 1 - Verify user log out after login
+//Test 1 - Verify user log out after registration
 
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
+import { RegistrationPage } from "../pages/RegistrationPage";
+
 import loginData from "../test-data/loginData.json" assert { type: "json" };
 import { ProfilePage } from "../pages/ProfilePage";
 
-test("Verify successfull post upload with valid data", async ({ page }) => {
+test("Verify user log out after login", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const profilePage = new ProfilePage(page);
 
@@ -17,6 +20,27 @@ test("Verify successfull post upload with valid data", async ({ page }) => {
   await loginPage.passwordFieldLocator.fill(loginData.validUser.password);
   await loginPage.rememberMeCheckboxLocator.check();
   await loginPage.click(loginPage.signInButtonLocator);
+
+  await profilePage.click(profilePage.profileButtonLocator);
+  await page.waitForURL("**/users/**");
+  await profilePage.click(profilePage.signOutButtonLocator);
+  await page.waitForURL("**/login**");
+
+  await expect(page.url()).toContain(loginPage.url);
+});
+
+test("Verify user log out after registration", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const profilePage = new ProfilePage(page);
+  const registrationPage = new RegistrationPage(page);
+
+  await loginPage.goToBasePage();
+  await loginPage.click(loginPage.loginButtonLocator);
+
+  await loginPage.click(loginPage.registerButtonLocator);
+
+  await registrationPage.fillRegistrationForm(validInput);
+  await registrationPage.submitRegistrationForm();
 
   await profilePage.click(profilePage.profileButtonLocator);
   await page.waitForURL("**/users/**");
