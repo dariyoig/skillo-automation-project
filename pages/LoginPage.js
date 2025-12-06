06b5d4e1bf8d8b27e40946b75ba5857acfb3f15f
Dariyoig <darioignatov72@gmail.com>
2025-12-06 14:55:37 +0200

    feat: added url property for LoginPage POM

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -5,15 +5,21 @@ export class LoginPage extends BasePage {
   constructor(page) {
     super(page);
 
-    this.signInTextLocator = page.getByRole("paragraph").filter({ hasText: "Sign in" });
+    this.url = "/users/login";
+
     this.usernameFieldLocator = page.locator("#defaultLoginFormUsername");
     this.passwordFieldLocator = page.locator("#defaultLoginFormPassword");
     this.rememberMeCheckboxLocator = page.getByRole("checkbox");
     this.signInButtonLocator = page.locator("#sign-in-button");
     this.registerButtonLocator = page.getByRole("link", { name: "Register" });
-    this.successfullLoginMessageLocator = page.getByRole("alertdialog", { name: "Successful login!" });
-    this.successfullLogoutMessageLocator = page.getByRole("alertdialog", { name: "Successful logout!" });
-    this.wrongUsernameOrPasswordMessageLocator =  page.getByRole('alertdialog', { name: 'Wrong username or password!' });
-
+    this.successfullLoginMessageLocator = page.getByRole("alertdialog", {
+      name: "Successful login!",
+    });
+    this.successfullLogoutMessageLocator = page.getByRole("alertdialog", {
+      name: "Successful logout!",
+    });
+    this.wrongUsernameOrPasswordMessageLocator = page.getByRole("alertdialog", {
+      name: "Wrong username or password!",
+    });
   }
 }
