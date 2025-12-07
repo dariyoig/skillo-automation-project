145a87bafea577e367e9ba33eb6a1a57358f30fb
Dariyoig <darioignatov72@gmail.com>
2025-12-07 15:26:08 +0200

    feat: added fixture for logged in user state

--- tests/fixtures/base.js
+++ tests/fixtures/base.js
@@ -1,5 +1,6 @@
 // Base extended test with common fixtures
 import { test as base, expect } from "@playwright/test";
+import loginData from "../../test-data/loginData.json" assert { type: "json" };
 
 import { LoginPage } from "../../pages/LoginPage";
 import { RegistrationPage } from "../../pages/RegistrationPage";
@@ -18,6 +19,11 @@ const test = base.extend({
   },
   profilePage: async ({ page }, use) => {
     await use(new ProfilePage(page));
+  },
+  loggedInUser: async ({ loginPage }, use) => {
+    await loginPage.page.goto(loginPage.url);
+    await loginPage.login(loginData.validUser.username, loginData.validUser.password, true);
+    await use();
   },
 });
 
