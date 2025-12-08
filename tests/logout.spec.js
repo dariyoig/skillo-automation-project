86f522ed4d1d81dc5682f3b69960037cac003b17
Dariyoig <darioignatov72@gmail.com>
2025-12-08 02:56:19 +0200

    refactor: reworked test to use POM sign out method + cosmetic changes

--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,23 +1,22 @@
-// // Logout tests specification file
-//Test 1 - Verify user log out from profile page after login
-//Test 2 - Verify user log out from profile page after registration
+// Verify user can log out successfuly after login
+// Verify user can log out successfuly after registration
 
 import { test, expect } from "./fixtures/base";
-import registrationData from "../test-data/registrationData.json" assert { type: "json" };
 import { makeRegistrationDataUnique } from "../utils/utils.js";
+import registrationData from "../test-data/registrationData.json" assert { type: "json" };
 
-test("Verify user log out from profile page after login", async ({ loginPage, loggedInUser }) => {
-  await loginPage.click(loginPage.signOutButtonLocator);
+test("Verify user can log out successfuly after login", async ({ loginPage, loggedInUser }) => {
+  await loginPage.signOut();
 
   await expect(loginPage.successfulLogoutMessageLocator).toBeVisible();
 });
 
-test("Verify user log out from profile page after registration", async ({ loginPage, registrationPage }) => {
+test("Verify user can log out successfuly after registration", async ({ loginPage, registrationPage }) => {
   const testData = makeRegistrationDataUnique({ ...registrationData.validInputs[0] });
 
   await loginPage.navigateToRegistrationPage();
   await registrationPage.register(testData);
-  await loginPage.click(loginPage.signOutButtonLocator);
+  await loginPage.signOut();
 
   await expect(loginPage.successfulLogoutMessageLocator).toBeVisible();
 });
